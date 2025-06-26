a3d2be866466fbb76490b4245fe2eeaf0761174f
Oluwafemi Ayinla <oluwafemi.ayinla@gmail.com>
2025-06-26 19:42:23 +0100

    create Sidebar & Map  component with initial layout and styles

---
+++ src/components/Map.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Map.module.css";
+
+export default function Map() {
+  return (
+    <div className={styles.mapContainer}>
+      <h1>MAP</h1>
+    </div>
+  );
+}
