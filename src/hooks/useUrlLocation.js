c47d201dccb4c207607eea980711c2bce6e5aa8b
Oluwafemi Ayinla <oluwafemi.ayinla@gmail.com>
2025-07-02 13:55:42 +0100

    feat: add and set up useUrlLocation hook for extracting lat/lng from URL in Form component

---
+++ src/hooks/useUrlLocation.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { useSearchParams } from "react-router-dom";
+
+export default function useUrlLocation() {
+  const [searchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  return [lat, lng];
+}
